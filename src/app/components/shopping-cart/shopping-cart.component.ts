387a5d30781df2882b8d6e7f32888896968238ab
JuanCarlos-27 <jcromero909@misena.edu.co>
2023-11-03 18:34:24 -0500

    Add localstorage products in cart

--- src/app/components/shopping-cart/shopping-cart.component.ts
+++ src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShoppingCartService } from 'src/app/services/shopping-cart.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -12,6 +12,24 @@ import { RouterLink } from '@angular/router';
 export default class ShoppingCartComponent {
 
   private shoppingCartService = inject(ShoppingCartService);
+  private router = inject(Router);
+
+  public productsInCart = this.shoppingCartService.productsInCart;
   public totalProductsInCart = this.shoppingCartService.totalProductsInCart;
+  public totalPayment = this.shoppingCartService.totalPayment;
+
+  redirectToProductDetail(id: number, event: Event) {
+    if (event.target instanceof HTMLButtonElement) return;
+    this.router.navigate(['/items', id])
+  }
+
+  deleteProductFormCart(id: number) {
+    this.shoppingCartService.deleteProductFormCart(id);
+  }
+
+  goBack() {
+    this.router.navigate(['/items'], { queryParams: { search: '' } })
+  }
+
 
 }
--- src/app/services/shopping-cart.service.ts
+++ src/app/services/shopping-cart.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, inject, signal, computed } from '@angular/core';
+import { Injectable, inject, signal, computed, effect } from '@angular/core';
 import { Product } from '../models/product.interface';
 import { ToastrService } from 'ngx-toastr';
 
@@ -7,16 +7,31 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class ShoppingCartService {
 
-  private toastr = inject(ToastrService)
+  constructor() {
+    effect(() => {
+      if (this._productsInCart()) this.saveLocalStorage();
+    })
 
-  private productsInCart = signal<Product[]>([]);
+    this.loadLocalStorage();
+  }
+
+  private toastr = inject(ToastrService)
+  private _productsInCart = signal<Product[]>([]);
 
   public totalProductsInCart = computed(() => {
-    return this.productsInCart().length
+    return this._productsInCart().length
   });
 
+  public totalPayment = computed(() => {
+    return this._productsInCart().reduce((acc, prod) => acc + prod.price! * prod.quantity!, 0);
+  });
+
+  get productsInCart() {
+    return this._productsInCart;
+  }
+
   addProductToCart(product: Product, quantity: number) {
-    const productExist = this.productsInCart().find(({ id }) => id === product.id);
+    const productExist = this._productsInCart().find(({ id }) => id === product.id);
 
     if (productExist && productExist.stock! < productExist.quantity! + quantity) {
       this.toastr.error('No hay suficiente stock');
@@ -24,7 +39,7 @@ export class ShoppingCartService {
     }
 
     if (productExist) {
-      this.productsInCart.update(products => (
+      this._productsInCart.update(products => (
         products.map(prod => {
           return prod.id === product.id
             ? { ...prod, quantity: prod.quantity! + quantity }
@@ -36,5 +51,20 @@ export class ShoppingCartService {
     }
 
     this.toastr.success(`${product.title} añadido al carrito!`);
+  }
+
+  deleteProductFormCart(id: number) {
+    this._productsInCart.update(products => products.filter(prod => prod.id !== id));
+  }
+
+  saveLocalStorage() {
+    localStorage.setItem('shoppingCart', JSON.stringify(this._productsInCart()));
+  }
+
+  loadLocalStorage() {
+    const productsInCart = JSON.parse(String(localStorage.getItem('shoppingCart')))
+    if (productsInCart) {
+      this._productsInCart.set(productsInCart);
+    }
   }
 }
